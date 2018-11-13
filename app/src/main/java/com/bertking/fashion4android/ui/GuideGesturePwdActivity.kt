3b6aa9d1cc16e37cf3f61394f6cb333eba428193
wangcheng <kingcheeng163@gmail.com>
2018-11-13 11:22:10 +0800

    add gesture password

---
+++ app/src/main/java/com/bertking/fashion4android/ui/GuideGesturePwdActivity.kt
@@ -0,0 +1,12 @@
+package com.bertking.fashion4android.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GuideGesturePwdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guide_gesture_pwd)
+    }
+}
